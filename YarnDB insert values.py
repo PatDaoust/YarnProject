14973b91c910474292c10f97e689791069b45442
Cataluna <catalunalilith2680@gmail.com>
2021-09-21 11:04:53 -0400

    inserted yarns and patterns ID 27000 to 27999 into ravelry_db

--- YarnDB insert values.py
+++ YarnDB insert values.py
@@ -277,7 +277,7 @@ if __name__ == "__main__":
         ravelry_adapter = HTTPAdapter(max_retries=3)
         a_session.mount('https://ravelry.com', ravelry_adapter)
 
-        for num in range(26000, 27000):
+        for num in range(27000, 28000):
             print(num)
             get_and_store_pattern(num)
             get_and_store_yarn(num)
